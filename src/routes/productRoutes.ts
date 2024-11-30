cc9295f8fab605d77ddfa322aa365061b1b9755e
Ciprian Miru <cipicip99@gmail.com>
2024-11-30 18:06:59 +0200

    products per page

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -6,22 +6,41 @@ const router = Router();
 
 // Fetch Products Route
 router.get('/', async (req, res) => {
+	const { page = 1, per_page = 30 } = req.query; // Default: page 1, 10 products per page
+
 	try {
-		const response = await wooCommerceApi.get('/products');
-		res.json(response.data);
+		const response = await wooCommerceApi.get(`/products`, {
+			params: {
+				page,
+				per_page,
+			},
+		});
+
+		// Send WooCommerce response to the client
+		res.status(200).json({
+			page: parseInt(page),
+			per_page: parseInt(per_page),
+			total: parseInt(response.headers['x-wp-total']),
+			total_pages: parseInt(response.headers['x-wp-totalpages']),
+			products: response.data,
+		});
 	} catch (error) {
-		//
-		console.log('here', error);
 		console.error('Error fetching products:', error.message);
-		res.status(500).json({ error: 'Failed to fetch products' });
+		res.status(error.response?.status || 500).json({
+			error: error.message || 'Internal Server Error',
+		});
 	}
 });
 
 router.get('/noutati', async (req, res) => {
 	try {
 		const response = await wooCommerceApi.get('/products', {
+			// limit to 10 products
+			// first 10 products
 			params: {
-				category: 15,
+				per_page: 10, // Limit to 10 products
+				orderby: 'date', // Order by creation date
+				order: 'desc', // Descending order (most recent first)
 			},
 		});
 		res.json(response.data);
