8cc546c7578f1761d60430a63f2aada3efb0ec83
Ciprian <cipicip99@gmail.com>
2025-08-25 10:06:33 +0300

    terst

--- src/apiSetup/wooCommerceApi.ts
+++ src/apiSetup/wooCommerceApi.ts
@@ -5,6 +5,7 @@
 import crypto from 'crypto';
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
 import dotenv from 'dotenv';
+import { paramsProduct } from '../routes/prodRoutes/prodUtils';
 
 dotenv.config();
 
@@ -29,8 +30,14 @@ const wooCommerceApi = api;
 // fetch all products from WooCommerce //
 export async function fetchWooCommerceProducts() {
 	try {
-		const response = await wooCommerceApi.get('products');
-		console.log('WooCommerce Products:', response.data);
+		const response = await wooCommerceApi.get('/products', {
+			// limit to 10 products
+			// first 10 products
+			per_page: 10, // Limit to 10 products
+			orderby: 'date', // Order by creation date
+			order: 'desc', // Descending order (most recent first)
+			...paramsProduct,
+		});
 		return response;
 	} catch (error) {
 		throw new Error(error);
